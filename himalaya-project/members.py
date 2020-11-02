3c7013864d2a8732ad671004e263486a28aad4bb
kyril wittouck <kyrilwittouck@gmail.com>
2020-11-02 18:29:40 +0100

    fct done

--- himalaya-project/members.py
+++ himalaya-project/members.py
@@ -1,26 +1,126 @@
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
+import datetime
 import os
 import pandas as pd
 
+class Members:
 
-data['summit_date1'] = pd.to_datetime(data.summit_date1, errors = 'coerce')
-data['death_date'] = pd.to_datetime(data.death_date, errors = 'coerce')
+    def get_data(self, data):
+        """
+        This function returns a Python DataFrame.
+        """
+        col_to_drop = ['first_name',
+               'last_name',
+               'age',
+               'birthdate',
+               'route2',
+               'route3',
+               'ascent2',
+               'ascent3',
+               'summit_time1',
+               'summit_time2',
+               'summit_time3',
+               'summit_date2',
+               'summit_date3',
+               'o2_note',
+               'death_date',
+               'death_time',
+               'hcn',
+               'mchksum']
 
-le = LabelEncoder()
-data['sex'] = le.fit_transform(data['sex'])
+        data.drop(columns= col_to_drop, inplace = True)
 
-preprocessor = ColumnTransformer([
-    ('median', SimpleImputer(strategy='median'), ['yob']),
-    ('frequency', SimpleImputer(strategy='most_frequent'), ['citizenship', 'status'])])
+        data['season'] = data['season'].map({
+                        0 : 'Unknown',
+                        1 : 'Spring',
+                        2 : 'Summer',
+                        3 : 'Autumn',
+                        4 : 'Winter'})
 
-# Pass the combined preprocessor into a Pipeline as a single step
-final_pipe = Pipeline([('preprocessing', preprocessor)])
+        data['death_type'] = data['death_type'].map({
+                        0 : 'Unspecified',
+                        1 : 'AMS',
+                        2 : 'Exhaustion',
+                        3 : 'Exposure / frostbite',
+                        4 : 'Fall',
+                        5 : 'Crevasse',
+                        6 : 'Icefall collapse',
+                        7 : 'Avalanche',
+                        8 : 'Falling rock / ice',
+                        9 : 'Disappearance',
+                        10 : 'Illness (non-AMS)',
+                        11 : 'Other',
+                        12 : 'Unknown'})
 
-tmp = final_pipe.fit_transform(data)
-data[['yob', 'citizenship', 'status']] = tmp
+        data['death_class'] = data['death_class'].map({
+                        0 : 'Unspecified',
+                        1 : 'Death enroute BC',
+                        2 : 'Death at BC / ABC',
+                        3 : 'Route preparation',
+                        4 : 'Ascending in summit bid',
+                        5 : 'Descending from summit bid',
+                        6 : 'Expedition evacuation',
+                        7 : 'Other / Unknown'})
 
-data.occupation.fillna('Unknown', inplace = True)
-data.residence.fillna('Unknown', inplace = True)
+        data['summit_bid'] = data['summit_bid'].map({
+                        0 : 'Unspecified',
+                        1 : 'No summit bid',
+                        2 : 'Aborted below high camp',
+                        3 : 'Aborted at high camp',
+                        4 : 'Aborted above high camp',
+                        5 : 'Successful summit bid'})
+
+        data.rename(columns={'summitter' : 'summit_term'}, inplace=True)
+
+        data['summit_term'] = data['summit_term'].map({
+                        0 : 'Unspecified',
+                        1 : 'Success',
+                        2 : 'Success subpeak',
+                        3 : 'Bad weather',
+                        4 : 'Bad conditions',
+                        5 : 'Accident',
+                        6 : 'AMS',
+                        7 : 'Exhaustion',
+                        8 : 'Frostbite',
+                        9 : 'Illnesses',
+                        10 : 'Logisitic',
+                        11 : 'O2 system failure',
+                        12 : 'Route difficulty',
+                        13 : 'Too late/slow',
+                        14 : 'Assisting',
+                        15 : 'Route/camp/rope preparation',
+                        16 : 'No time left',
+                        17 : 'No climbing',
+                        18 : 'Other',
+                        19 : 'Unknown'})
+
+        data['summit_date1'] = pd.to_datetime(data.summit_date1, errors = 'coerce')
+
+        le = LabelEncoder()
+        data['sex'] = le.fit_transform(data['sex'])
+
+        # Combine "age_transformer" and "One hot encoder" into a single preprocessor
+        preprocessor = ColumnTransformer([
+            ('median', SimpleImputer(strategy='median'), ['yob']),
+            ('frequency', SimpleImputer(strategy='most_frequent'), ['citizenship', 'status']),
+            ('ohe', OneHotEncoder(drop= 'first'), ['sex'])])
+
+        # Pass the combined preprocessor into a Pipeline as a single step
+        final_pipe = Pipeline([('preprocessing', preprocessor)])
+
+        data_trans = final_pipe.fit_transform(data)
+
+        data[['yob', 'citizenship', 'status', 'sex']] = data_trans
+
+        data.rename(columns={'sex' : 'sex_M'}, inplace=True)
+
+        data.occupation.fillna('Unknown', inplace = True)
+        data.residence.fillna('Unknown', inplace = True)
+
+        now_year = datetime.datetime.now().year
+        data['age'] = now_year - data['yob']
+
+        return data
